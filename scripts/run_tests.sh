769ad68dca9e2f91701d800a7741ea097919cbe7
Jakub Danek <jakub.danek@yoso.fi>
2013-05-15 20:06:53 +0200

    Documentation

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,6 +2,7 @@
 cd  `dirname "$0"`
 cd ..
 for var in 1 2 3 4 5 6 7 8 9 10
+#for var in 1
     do
 #        echo "ahoj" > ./logs/ahoj.log
         stdbuf -oL python test_runner.py > ./logs/test_result${var}.log 2>./logs/test_result${var}.err
